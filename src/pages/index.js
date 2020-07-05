9775ed86a95dda08004acc468311e7419d63c7bc
Ben <me@benpaat.xyz>
2020-07-05 14:19:32 +0800

    💄Rename to themeselector
    It now has feature to have more than 2 theme selection

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,13 @@ import '../styles/style.scss'
 import Layout from '../components/Layout'
 import GitmojiList from '../components/GitmojiList'
 import SEO from '../components/SEO'
-import Toggle from '../components/Toggle'
+import ThemeSelector from '../components/ThemeSelector'
 import { gitmojis } from '../data/gitmojis.json'
 
 const Home = () => (
   <>
     <SEO />
-    <Toggle />
+    <ThemeSelector />
     <Layout headerWithSocialButtons>
       <GitmojiList gitmojis={gitmojis} />
     </Layout>
